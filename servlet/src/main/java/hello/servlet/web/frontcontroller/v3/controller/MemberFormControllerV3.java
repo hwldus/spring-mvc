a07785f82ed0043914a54e75876df5c2b15dfe9f
JiYeon <89963270+hwldus@users.noreply.github.com>
2023-01-31 02:42:38 +0900

    fron-controller v4
    - model 객체 생성이 아닌 파라미터로 전달
    - v3 오타 수정

--- servlet/src/main/java/hello/servlet/web/frontcontroller/v3/controller/MemberFormControllerV3.java
+++ servlet/src/main/java/hello/servlet/web/frontcontroller/v3/controller/MemberFormControllerV3.java
@@ -8,7 +8,7 @@ import java.util.Map;
 public class MemberFormControllerV3 implements ControllerV3 {
 
     @Override
-    public ModelView process(Map<String, String> pramMap) {
+    public ModelView process(Map<String, String> paramMap) {
         return new ModelView("new-form"); //논리 이름주소
     }
 }
